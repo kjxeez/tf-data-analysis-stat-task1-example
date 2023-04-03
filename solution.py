957bee318ea3e56d7d5485fdd6218b385d843197
изабелла акопян <82978703+kjxeez@users.noreply.github.com>
2023-04-03 19:20:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,8 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 584664949 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    mu = np.mean(x)
+    return mu/32 # Ваш ответ
